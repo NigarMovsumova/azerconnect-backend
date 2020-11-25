89d0c5556157ab9692f75ac1d6e0177de81dd581
Nigar <movsum.nigar@gmail.com>
2020-11-25 15:50:19 +0400

    most crucial tests are added (will add more)

--- src/main/java/az/azerconnect/azerconnectbackend/service/impl/EligibilityCheckServiceImpl.java
+++ src/main/java/az/azerconnect/azerconnectbackend/service/impl/EligibilityCheckServiceImpl.java
@@ -1,40 +1,31 @@
 package az.azerconnect.azerconnectbackend.service.impl;
 
+import az.azerconnect.azerconnectbackend.exception.EmptyRequestException;
 import az.azerconnect.azerconnectbackend.exception.NullException;
 import az.azerconnect.azerconnectbackend.model.MsisdnRequest;
 import az.azerconnect.azerconnectbackend.service.EligibilityCheckService;
 import java.util.Map;
-import org.apache.commons.lang3.StringUtils;
+import java.util.regex.Pattern;
 import org.springframework.stereotype.Service;
 
 import static az.azerconnect.azerconnectbackend.util.MsisdnUtil.checkEligibility;
+import static az.azerconnect.azerconnectbackend.util.MsisdnUtil.createRegexPattern;
 
 @Service
 public class EligibilityCheckServiceImpl implements EligibilityCheckService {
 
-    private static String createRegexPattern(String listString) {
-        if (listString == null) {
-            listString = "$^";
-        }
-        return StringUtils.chop(
-                listString
-                        .replace("%,", "|")
-                        .replace("_", "([0-9])")
-                        .replace(",", "|"));
-    }
-
     @Override
     public Map<String, String> isEligibleToSell(MsisdnRequest msisdnRequest) {
         if (msisdnRequest == null) {
-            throw new NullException("Your request is empty");
+            throw new EmptyRequestException("Your request is empty");
         }
 
         if (msisdnRequest.getMsisdnList() == null || msisdnRequest.getMsisdnList().isEmpty()) {
-            throw new NullException("Number List is not provided");
+            throw new NullException("Number list is not provided");
         }
 
-        String whiteListRegexPattern = createRegexPattern(msisdnRequest.getWhiteListString());
-        String blackListRegexPattern = createRegexPattern(msisdnRequest.getBlackListString());
+        Pattern whiteListRegexPattern = createRegexPattern(msisdnRequest.getWhiteListString());
+        Pattern blackListRegexPattern = createRegexPattern(msisdnRequest.getBlackListString());
         return checkEligibility(msisdnRequest.getMsisdnList(), whiteListRegexPattern, blackListRegexPattern);
     }
 }
--- src/main/java/az/azerconnect/azerconnectbackend/util/MsisdnUtil.java
+++ src/main/java/az/azerconnect/azerconnectbackend/util/MsisdnUtil.java
@@ -4,6 +4,7 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.regex.Pattern;
+import org.apache.commons.lang3.StringUtils;
 
 import static java.lang.String.format;
 
@@ -11,21 +12,19 @@ public class MsisdnUtil {
 
 
     public static Map<String, String> checkEligibility(List<String> msisdnList,
-                                                       String whiteListPattern,
-                                                       String blackListPattern) {
+                                                       Pattern whiteListPattern,
+                                                       Pattern blackListPattern) {
         Map<String, String> eligibilityMap = new HashMap<>();
 
         for (String msisdn : msisdnList) {
             msisdn = msisdn.substring(3);
             if (isMsisdnValid(msisdn)) {
-                if (!Pattern.compile(whiteListPattern).matcher(msisdn).lookingAt()) {
-                    eligibilityMap.put(msisdn, format("msisdn = %s is not in whitelist", msisdn));
-                }
-                if (Pattern.compile(blackListPattern).matcher(msisdn).lookingAt()) {
+                if (blackListPattern.matcher(msisdn).lookingAt()) {
                     eligibilityMap.put(msisdn, format("msisdn = %s is in blacklist", msisdn));
-                }
-                if (Pattern.compile(whiteListPattern).matcher(msisdn).lookingAt() &&
-                        !Pattern.compile(blackListPattern).matcher(msisdn).lookingAt()) {
+                } else if (!whiteListPattern.matcher(msisdn).lookingAt()) {
+                    eligibilityMap.put(msisdn, format("msisdn = %s is not in whitelist", msisdn));
+                } else if (whiteListPattern.matcher(msisdn).lookingAt() &&
+                        !blackListPattern.matcher(msisdn).lookingAt()) {
                     eligibilityMap.put(msisdn, "OK");
                 }
             }
@@ -33,11 +32,22 @@ public class MsisdnUtil {
         return eligibilityMap;
     }
 
+    public static Pattern createRegexPattern(String listString) {
+        if (listString == null) {
+            listString = "$^";
+        }
+
+        String regexString = listString.replace("%,", "|").replace("_", "([0-9])")
+                .replace(",", "|");
+        if (regexString.endsWith("|")) {
+            regexString = StringUtils.chop(regexString);
+        }
+        return Pattern.compile(regexString);
+    }
+
 
     public static boolean isMsisdnValid(String msisdn) {
-        if (msisdn == null) {
-            return false;
-        }
+        if (msisdn == null) return false;
         return Pattern.compile("(70|77|55|99)[0-9]{7}").matcher(msisdn).matches();
     }
 }
