209288759490cd85a32051f780753b9e25b74489
Nigar <movsum.nigar@gmail.com>
2020-11-25 12:02:13 +0400

    added CrossOrigin

--- src/main/java/az/azerconnect/azerconnectbackend/controller/EligibilityController.java
+++ src/main/java/az/azerconnect/azerconnectbackend/controller/EligibilityController.java
@@ -3,11 +3,13 @@ package az.azerconnect.azerconnectbackend.controller;
 import az.azerconnect.azerconnectbackend.model.MsisdnRequest;
 import az.azerconnect.azerconnectbackend.service.EligibilityCheckService;
 import java.util.Map;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 @RequestMapping("${api.root.url}")
 public class EligibilityController {
 
